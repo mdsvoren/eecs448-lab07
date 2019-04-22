0d6eb690c0ee283b65fc116d75cbca0e26272cc2
Michael Svoren <michael.svoren@gmail.com>
2019-04-22 09:36:18 -0500

    change logical not

--- testSuite.cpp
+++ testSuite.cpp
@@ -458,7 +458,7 @@ bool testSuite::testRemoveFront()
   //check if it actually removes
   LinkedListOfInts l1;
   l1.addBack(1);
-  if (!l1.size()==1)
+  if (l1.size()!=1)
   {
     std::cout<<"ERROR: testRemoveFront: unable to test due to failed addBack function\n";
     pass = false;
